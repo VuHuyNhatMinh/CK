414f104be1c9a9a0d40bc5d8337f513adb9719e3
VuHuyNhatMinh <nminhstickpk@gmail.com>
2022-03-02 19:42:51 +0700

    0203

--- ErrorChecking.h
+++ ErrorChecking.h
@@ -48,118 +48,6 @@ bool is_hex(char* text, int size, char a[])    //input là một string
     return true;
 }
 
-//chuyển binary ở dạng string sang số decimal
-int bin_to_dec(string bin) 
-{
-    int res=0;
-    for(int i=0;i<=bin.size()-1;i++)
-    {
-        res+=(bin[i]-48)*pow(2,bin.size()-1-i);
-    }
-    return res;
-}
-
-
-//chuyển một số decimal sang số binary ở dạng string
-string dec_to_bin(int value)
-{   
-    string res;
-    while(value!=0)
-    {
-        if(value%2 == 0)
-        {
-            res+='0';
-        }
-        else
-        {
-            res+='1';
-        }
-        value/=2;
-    }
-    string reverse_res="";
-    for(int i=res.size()-1;i>=0;i--)
-    {
-        reverse_res+=res[i];
-    }
-    return reverse_res;
-}
-
-
-//chuyển số decimal sang số hex ở dạng string
-string dec_to_hex(int value)
-{
-    string res = "";
-    string reverse_res = ""; 
-    while(value!=0)
-    {
-       int carry = value%16;
-       if(carry<=9)
-       {
-           res+=char(carry+48);
-       }
-       else
-       {
-           res+=char(carry-10+'A');
-       }
-       value/=16;
-    }
-    for(int i=res.size()-1;i>=0;i--)
-    {
-        reverse_res+=res[i]; 
-    }
-    return reverse_res;
-
-}
-
-
-//chuyển số hex ở dạng string sang số decimal
-int hex_to_dec(string hex)   //hàm truyền vào là một string hex, đầu ra là decimal
-{
-    int res = 0;
-    for(int i=0;i<hex.length();i++)
-    {
-        if(48<=hex[i]&&hex[i]<=57)
-        {
-            res += (hex[i]-48)*pow(16,hex.length()-i-1);
-        }
-        else if(65<=hex[i]&&hex[i]<=70)
-        {
-            res += (hex[i]-55)*pow(16,hex.length()-i-1);
-        }
-    }
-    return res;
-}
-
-
-
-//đảo tất cả các bit của một binary ở dạng string
-string inverse(string text)         //đầu vào là binary ở dạng string, đầu ra là binary ở dạng string
-{
-    for(int k=0; k < text.size();k++)
-    {
-        if(text[k]=='0')
-        {
-            text[k]='1';
-        }
-        else
-        {
-            text[k]='0';
-        }
-    }
-    return text;
-}
-
-
-
-//Lấy 2 kí tự cuối cùng của một string 
-string get2LastBit(string text)         
-{
-    string temp;
-    temp += text[text.size()-2];
-    temp += text[text.size()-1]; 
-    return temp;
-}
-
 //check các lỗi như checksum, lỗi count_byte, lỗi option_byte
 //đầu vào là string tất cả data, trả về true nếu không lỗi, trả về false + in ra loại lỗi ở dòng nào đó (lỗi ở dòng đầu tiên) 
 bool check_error_line_by_line(char* data, int size)
@@ -226,6 +114,8 @@ bool check_error_line_by_line(char* data, int size)
         }
 
         string error_check = "";                                //check count_byte của dòng đang xét có nhỏ hơn 32 bits không
+        
+        /*
         error_check += data_temp[0];
         error_check += data_temp[1];
         if(hex_to_dec(error_check) > 32)
@@ -233,7 +123,8 @@ bool check_error_line_by_line(char* data, int size)
             cout << "Error: Out of 32 bits range in "<< line;
             return false;
         }
-
+        */
+       
         error_check = "";                                       //check option_byte của dòng đang xét xem có thỏa mãn không
         error_check += data_temp[6];
         error_check += data_temp[7];
--- GenerateData.h
+++ GenerateData.h
@@ -1,7 +1,6 @@
 /***********************************************************************
  * About: This is the program for processing data.
  * Purpose: get data from file, convert from dec to hex and vice versa.
- * Author: Vu Huy Nhat Minh 20191973
 ***********************************************************************/
 
 #ifndef _GENERATE_DATA_
@@ -65,6 +64,119 @@ string separateLine(char* text, int size)
     }
     return line;
 }
+
+//chuyển binary ở dạng string sang số decimal
+int bin_to_dec(string bin) 
+{
+    int res=0;
+    for(int i=0;i<=bin.size()-1;i++)
+    {
+        res+=(bin[i]-48)*pow(2,bin.size()-1-i);
+    }
+    return res;
+}
+
+
+//chuyển một số decimal sang số binary ở dạng string
+string dec_to_bin(int value)
+{   
+    string res;
+    while(value!=0)
+    {
+        if(value%2 == 0)
+        {
+            res+='0';
+        }
+        else
+        {
+            res+='1';
+        }
+        value/=2;
+    }
+    string reverse_res="";
+    for(int i=res.size()-1;i>=0;i--)
+    {
+        reverse_res+=res[i];
+    }
+    return reverse_res;
+}
+
+
+//chuyển số decimal sang số hex ở dạng string
+string dec_to_hex(int value)
+{
+    string res = "";
+    string reverse_res = ""; 
+    while(value!=0)
+    {
+       int carry = value%16;
+       if(carry<=9)
+       {
+           res+=char(carry+48);
+       }
+       else
+       {
+           res+=char(carry-10+'A');
+       }
+       value/=16;
+    }
+    for(int i=res.size()-1;i>=0;i--)
+    {
+        reverse_res+=res[i]; 
+    }
+    return reverse_res;
+
+}
+
+
+//chuyển số hex ở dạng string sang số decimal
+int hex_to_dec(string hex)   //hàm truyền vào là một string hex, đầu ra là decimal
+{
+    int res = 0;
+    for(int i=0;i<hex.length();i++)
+    {
+        if(48<=hex[i]&&hex[i]<=57)
+        {
+            res += (hex[i]-48)*pow(16,hex.length()-i-1);
+        }
+        else if(65<=hex[i]&&hex[i]<=70)
+        {
+            res += (hex[i]-55)*pow(16,hex.length()-i-1);
+        }
+    }
+    return res;
+}
+
+
+
+//đảo tất cả các bit của một binary ở dạng string
+string inverse(string text)         //đầu vào là binary ở dạng string, đầu ra là binary ở dạng string
+{
+    for(int k=0; k < text.size();k++)
+    {
+        if(text[k]=='0')
+        {
+            text[k]='1';
+        }
+        else
+        {
+            text[k]='0';
+        }
+    }
+    return text;
+}
+
+
+
+//Lấy 2 kí tự cuối cùng của một string 
+string get2LastBit(string text)         
+{
+    string temp;
+    temp += text[text.size()-2];
+    temp += text[text.size()-1]; 
+    return temp;
+}
+
 
 /*******************************************
  * About: Convert single character (hexadecimal) to integer (decimal).
